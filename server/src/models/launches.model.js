d9905b9f13c0cc9d80c55ce34f4371385817b93f
lavishsehgal4 <sehgallavish5@gmail.com>
2025-10-28 21:44:23 +0530

    added delete route

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,11 +7,15 @@ const launch = {
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   target: "Kepler-442 b",
-  customer: ["ZTM", "NASA"],
+  customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
 launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
 
 function getAllLaunches() {
   return Array.from(launches.values());
@@ -23,14 +27,23 @@ function addNewLaunch(launch) {
     latestFlightNumber,
     Object.assign(launch, {
       flightNumber: latestFlightNumber,
-      customer: ["ZTM", "NASA"],
+      customers: ["ZTM", "NASA"],
       upcoming: true,
       success: true,
     })
   );
 }
 
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
 module.exports = {
   getAllLaunches,
   addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
 };
